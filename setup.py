92ad3a58fbf89646c68d777ad4e74016f25fefb7
yhql <victor_servant@hotmail.com>
2021-09-14 16:00:58 +0200

    provide ability to save traces and labels

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="visplot",
-    install_requires=[
-        "vispy",
-        "numpy",
-    ],
+    install_requires=["pyqt5", "vispy", "numpy"],
     packages=find_packages(),
     py_modules=["visplot"],
     version=1.0,
