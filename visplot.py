cfd0095538043eea33fa1125af8847c6af440213
Michael Mouchous <michael.mouchous@ledger.fr>
2022-08-08 10:41:18 +0200

    A pass of black

--- visplot.py
+++ visplot.py
@@ -1,26 +1,26 @@
-
 from itertools import cycle
 from typing import Optional, Tuple
 import numpy as np
 from vispy import scene
 from vispy import color
-from vispy import util 
+from vispy import util
 
 
 class plot:
     """
-    Fast plot of many large traces as a single object, using vispy. 
+    Fast plot of many large traces as a single object, using vispy.
     """
-    MAX_HL = 12 
+
+    MAX_HL = 12
     BG_DARK = "#222"
     LBL_POS_DEFAULTX = 170
     LBL_POS_DEFAULTY = 40
-    LBL_SPACING = 16 
+    LBL_SPACING = 16
 
     def __init__(
         self, curves=None, labels=None, bgcolor=BG_DARK, parent=None, dontrun=False
     ):
-        """ 
+        """
         :param icurves: input curve or list of curves
         :param clrmap: (optional) what colormap name from vispy.colormap to use
         """
@@ -46,8 +46,8 @@ class plot:
         self.x_axis.link_view(self.view)
         self.y_axis.link_view(self.view)
 
-        self.ctrl_pressed = False 
-        self.shift_pressed = False 
+        self.ctrl_pressed = False
+        self.shift_pressed = False
         self.alt_pressed = False
 
         self.canvas.connect(self.on_key_press)
@@ -58,7 +58,7 @@ class plot:
         # This value stores the mouse position between calls of mouse move as a tuple
         self._init_pos: Optional[Tuple[int, int]] = None
         self.canvas.connect(self.on_mouse_move)
-        
+
         if curves is not None:
             self.draw_curves(curves, labels)
 
@@ -69,7 +69,7 @@ class plot:
     def clear(self):
         if self.line is not None:
             self.line.parent = None
-        self.selected_lines = [] 
+        self.selected_lines = []
         self.hl_labels = []
 
     def draw_curves(self, curves_, labels=None, clrmap="husl"):
@@ -103,28 +103,32 @@ class plot:
         for i in range(size, nb_traces * size, size):
             connect[i - 1, 1] = i - 1
 
-        self.colors = np.ones((nb_traces*size,3),dtype=np.float32)
-        self.backup_colors = np.ones((nb_traces,3),dtype=np.float32)
+        self.colors = np.ones((nb_traces * size, 3), dtype=np.float32)
+        self.backup_colors = np.ones((nb_traces, 3), dtype=np.float32)
 
-        R_p = np.linspace(0.4,0.4,num=nb_traces)
-        G_p = np.linspace(0.5,0.3,num=nb_traces)
-        B_p = np.linspace(0.5,0.3,num=nb_traces)
+        R_p = np.linspace(0.4, 0.4, num=nb_traces)
+        G_p = np.linspace(0.5, 0.3, num=nb_traces)
+        B_p = np.linspace(0.5, 0.3, num=nb_traces)
 
-        self.colors[:,0] = np.repeat(R_p,size)
-        self.colors[:,1] = np.repeat(G_p,size)
-        self.colors[:,2] = np.repeat(B_p,size)
+        self.colors[:, 0] = np.repeat(R_p, size)
+        self.colors[:, 1] = np.repeat(G_p, size)
+        self.colors[:, 2] = np.repeat(B_p, size)
 
-        self.backup_colors[:,0] = R_p
-        self.backup_colors[:,1] = G_p
-        self.backup_colors[:,2] = B_p
+        self.backup_colors[:, 0] = R_p
+        self.backup_colors[:, 1] = G_p
+        self.backup_colors[:, 2] = B_p
 
-        self.line = scene.Line(pos=xy_curves, color=self.colors, parent=self.view.scene, connect=connect)
+        self.line = scene.Line(
+            pos=xy_curves, color=self.colors, parent=self.view.scene, connect=connect
+        )
 
-        self.selected_lines = [] 
+        self.selected_lines = []
         # To store the lines offsets applied with the "shift"
         self.lines_offset = {}
         self.hl_labels = []
-        self.hl_colorset = cycle(color.get_colormap(clrmap)[np.linspace(0.0, 1.0, self.MAX_HL)])
+        self.hl_colorset = cycle(
+            color.get_colormap(clrmap)[np.linspace(0.0, 1.0, self.MAX_HL)]
+        )
 
         self.view.camera.set_range(x=(-1, size), y=(curves.min(), curves.max()))
 
@@ -136,28 +140,28 @@ class plot:
 
         tr = self.canvas.scene.node_transform(self.view.scene)
         # Canvas coordinates of clicked point
-        x1,y1,_,_ = tr.map((x,y))
+        x1, y1, _, _ = tr.map((x, y))
         # Canvas coordinates of upper right corner of bounding box
         # containing clicked point
-        x2,max_y,_,_ = tr.map((x+radius,y+radius))
+        x2, max_y, _, _ = tr.map((x + radius, y + radius))
 
-        _,min_y,_,_ = tr.map((x, y-radius))
+        _, min_y, _, _ = tr.map((x, y - radius))
         min_y, max_y = min(min_y, max_y), max(min_y, max_y)
 
         # Gather all segments left and right of the clicked point
         rx = int(round(x1))
-        tab = self.line.pos[:,rx-radius:rx+radius]
+        tab = self.line.pos[:, rx - radius : rx + radius]
 
-        # Find closest point, filtering out points whose 
+        # Find closest point, filtering out points whose
         # y-coordinate is outside the bounding box around
         # the clicked point
-        max_norm = 1000000 
-        imin = None 
-        f = np.array([x1,y1], dtype=np.float32)
-        for i,s in enumerate(tab):
+        max_norm = 1000000
+        imin = None
+        f = np.array([x1, y1], dtype=np.float32)
+        for i, s in enumerate(tab):
             for p in s:
-                if min_y<p[1]<max_y:
-                    t = np.linalg.norm(f-p)
+                if min_y < p[1] < max_y:
+                    t = np.linalg.norm(f - p)
                     if t < max_norm:
                         max_norm = t
                         imin = i
@@ -165,23 +169,23 @@ class plot:
         # this is the index of the closest line
         # or None if there are no point in the
         # defined area
-        return imin 
+        return imin
 
     def on_key_press(self, event):
-        if event.key == 'Control':
+        if event.key == "Control":
             self.ctrl_pressed = True
-        if event.key == 'Shift':
+        if event.key == "Shift":
             self.shift_pressed = True
             self._init_pos = None
-        if event.key == 'Alt':
+        if event.key == "Alt":
             self.alt_pressed = True
 
     def on_key_release(self, event):
-        if event.key == 'Control':
-            self.ctrl_pressed = False 
-        if event.key == 'Shift':
-            self.shift_pressed = False 
-        if event.key == 'Alt':
+        if event.key == "Control":
+            self.ctrl_pressed = False
+        if event.key == "Shift":
+            self.shift_pressed = False
+        if event.key == "Alt":
             self.alt_pressed = False
 
     def on_mouse_press(self, event):
@@ -211,26 +215,30 @@ class plot:
         :param curve_no: The curve identifier to apply the offset
         :param offset: The displacement to apply to the curve.
         """
-        self.line.pos[curve_no][:,0] += offset[0]
-        self.line.pos[curve_no][:,1] += offset[1]
+        self.line.pos[curve_no][:, 0] += offset[0]
+        self.line.pos[curve_no][:, 1] += offset[1]
         self.line.set_data(pos=self.line.pos)
         curve_offset = self.lines_offset.get(curve_no, [0.0, 0.0])
-        self.lines_offset[curve_no] = [offset[0] + curve_offset[0], offset[1] + curve_offset[1]]
+        self.lines_offset[curve_no] = [
+            offset[0] + curve_offset[0],
+            offset[1] + curve_offset[1],
+        ]
 
-    def on_mouse_move(self,event):
+    def on_mouse_move(self, event):
         if self.shift_pressed:
             if len(self.selected_lines) > 0:
                 if self._init_pos is None:
                     self._init_pos = event.pos
                 # map to screen displacement
                 tr = self.canvas.scene.node_transform(self.view.scene)
-                x,y,_,_ = tr.map(event.pos)
-                init_x,init_y,_,_ = tr.map(self._init_pos)
+                x, y, _, _ = tr.map(event.pos)
+                init_x, init_y, _, _ = tr.map(self._init_pos)
                 delta_x = x - init_x
                 delta_y = y - init_y
                 for curve_no in self.selected_lines:
-                    self.apply_offset(curve_no,
-                                      (0.0, delta_y) if self.alt_pressed else (delta_x, 0.0))
+                    self.apply_offset(
+                        curve_no, (0.0, delta_y) if self.alt_pressed else (delta_x, 0.0)
+                    )
                 self._init_pos = event.pos
                 self.canvas.update()
 
@@ -239,13 +247,13 @@ class plot:
         if self.shift_pressed:
             return
 
-        x,y = event.pos
+        x, y = event.pos
 
         # if released more than 3 pixels away from click (i.e. dragging), ignore
-        if not (abs(x-self._init_pos[0])<3 and abs(y-self._init_pos[1])<3):
+        if not (abs(x - self._init_pos[0]) < 3 and abs(y - self._init_pos[1]) < 3):
             return
 
-        closest_line = self.find_closest_line(x,y)
+        closest_line = self.find_closest_line(x, y)
         if closest_line is None:
             return
 
@@ -253,10 +261,18 @@ class plot:
             self.multiple_select(closest_line)
         else:
             self.single_select(closest_line)
-                
+
     def _add_label(self, curve_index, new_color):
-        new_label = scene.Text(f"{self.labels[curve_index]}", color=new_color, anchor_x = 'left', parent=self.canvas.scene)
-        new_label.pos = self.LBL_POS_DEFAULTX, self.LBL_POS_DEFAULTY + self.LBL_SPACING * len(self.hl_labels)
+        new_label = scene.Text(
+            f"{self.labels[curve_index]}",
+            color=new_color,
+            anchor_x="left",
+            parent=self.canvas.scene,
+        )
+        new_label.pos = (
+            self.LBL_POS_DEFAULTX,
+            self.LBL_POS_DEFAULTY + self.LBL_SPACING * len(self.hl_labels),
+        )
         self.hl_labels.append((curve_index, new_label))
 
     def _del_label_from_curve_index(self, curve_index):
@@ -266,20 +282,24 @@ class plot:
 
         ## redraw text items
         for i, lbl in enumerate(self.hl_labels[idx:]):
-            lbl[1].pos = self.LBL_POS_DEFAULTX, self.LBL_POS_DEFAULTY + self.LBL_SPACING * (idx+i)
+            lbl[
+                1
+            ].pos = self.LBL_POS_DEFAULTX, self.LBL_POS_DEFAULTY + self.LBL_SPACING * (
+                idx + i
+            )
 
     def _find_label_from_curve_index(self, curve_index):
-        return list(map(lambda x:x[0], self.hl_labels)).index(curve_index)
+        return list(map(lambda x: x[0], self.hl_labels)).index(curve_index)
 
-    def _set_curve_color(self,n, new_color):
+    def _set_curve_color(self, n, new_color):
         _, S = self.shape_
         a = n * S
-        self.colors[a:a+S] = np.repeat(new_color.rgb, S, axis=0)
+        self.colors[a : a + S] = np.repeat(new_color.rgb, S, axis=0)
 
-    def _restore_nth_curve_color(self,n):
+    def _restore_nth_curve_color(self, n):
         _, S = self.shape_
         nnx = n * S
-        self.colors[nnx:nnx+S] = np.repeat([self.backup_colors[n]], S, axis=0)
+        self.colors[nnx : nnx + S] = np.repeat([self.backup_colors[n]], S, axis=0)
 
     def single_select(self, curve_index):
         # Unselect previously highlighted curves
@@ -293,12 +313,12 @@ class plot:
         self.hl_labels = []
 
         # Display its index/label
-        new_color = next(self.hl_colorset)               # Pick a new color
+        new_color = next(self.hl_colorset)  # Pick a new color
         self._add_label(curve_index, new_color)
-        self.selected_lines = [curve_index]              # Add this curve to the selected batch
-        self._set_curve_color(curve_index, new_color)    # Set its new color
+        self.selected_lines = [curve_index]  # Add this curve to the selected batch
+        self._set_curve_color(curve_index, new_color)  # Set its new color
 
-        self.line.set_data(color=self.colors)            # Update colors
+        self.line.set_data(color=self.colors)  # Update colors
 
     def multiple_select(self, curve_index):
         if curve_index in self.selected_lines:
@@ -310,12 +330,12 @@ class plot:
             self._restore_nth_curve_color(curve_index)
             self.selected_lines.remove(curve_index)
         else:
-            N,S = self.shape_
+            N, S = self.shape_
             new_color = next(self.hl_colorset)
             self._add_label(curve_index, new_color)
             self.selected_lines.append(curve_index)
             self._set_curve_color(curve_index, new_color)
-        
+
         self.line.set_data(color=self.colors)
 
     def add_horizontal_ruler(self, y: float) -> scene.visuals.InfiniteLine:
@@ -328,15 +348,12 @@ class plot:
         )
 
     def add_horizontal_band(self, y0: float, y1: float) -> scene.visuals.Polygon:
-        """ Add a horizontal band (rectangle) covering 'y0' to 'y1' on the canvas. """
+        """Add a horizontal band (rectangle) covering 'y0' to 'y1' on the canvas."""
         _, size = self.shape_
-        coords = [
-            (0, y0),
-            (0, y1),
-            (size, y1),
-            (size, y0)
-        ]
-        return scene.visuals.Polygon(coords, color=color.Color('#ddd', alpha=0.1), parent=self.view.scene)
+        coords = [(0, y0), (0, y1), (size, y1), (size, y0)]
+        return scene.visuals.Polygon(
+            coords, color=color.Color("#ddd", alpha=0.1), parent=self.view.scene
+        )
 
     def add_vertical_ruler(self, x: float) -> scene.visuals.InfiniteLine:
         """Add a single light grey vertical line at position 'x' on the canvas."""
@@ -347,13 +364,17 @@ class plot:
             vertical=True,
         )
 
+
 if __name__ == "__main__":
     N = 50
-    a = [i/10*np.sin(np.linspace(0.0+i/10,10.0+i/10,num=2000)) for i in range(N)]
+    a = [
+        i / 10 * np.sin(np.linspace(0.0 + i / 10, 10.0 + i / 10, num=2000))
+        for i in range(N)
+    ]
     v = plot(a, dontrun=True)
     v.multiple_select(4)
     v.multiple_select(7)
     v.add_horizontal_ruler(2.2)
-    v.add_horizontal_band(1., -1.)
+    v.add_horizontal_band(1.0, -1.0)
     v.add_vertical_ruler(10)
     v.run()
